7b4fd9599b9afb8092ac1b25e8669443dd28e0ef
JeongJinLee-github <51868872+JeongJinLee-github@users.noreply.github.com>
2020-06-22 23:50:44 +0900

    study Class of JavaScript in ramfore

--- javascript/class.js
+++ javascript/class.js
@@ -66,15 +66,65 @@ class Article {
     constructor(articleNumber) {
         this.articleNumber = articleNumber;
     }
+    publisher_c = 'fieldClass ';
 
     static printPublisher() {
         console.log(Article.publisher);
     }
+    printPublisher_c() {
+        console.log(Article.publisher);
+    }
 }
 
-const article1 = new Article(1);
-const article2 = new Article(2);
-console.log(article1.publisher);
-console.log(article2.publisher);
+const article1 = new Article;
+const article2 = new Article;
+console.log(article1.publisher_c);
+console.log(article2.publisher_c);
+article1.printPublisher_c();
+article2.printPublisher_c();
 console.log(Article.publisher);
 Article.printPublisher();
+
+// 5. Inheritance
+// a way for one classto extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`△ ■ ○ drawing ${this.color} color !!`);
+    }
+    getArray() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}  // extends is Inheritance
+class Triangle extends Shape {    // extends is Inheritance
+    draw() {
+        super.draw();
+        console.log('▲');
+    }
+    
+    getArray() {                  // overriding
+        return this.width * this.height / 2;
+    }
+}  
+
+const rectangle = new Rectangle(20, 30, 'blue');
+rectangle.draw();
+console.log(`Rectangle Array is ${rectangle.getArray()}`);
+const triangle = new Triangle(20, 30, 'blue');
+triangle.draw()
+console.log(`Trangle Array is ${triangle.getArray()}`);
+
+
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Object);
