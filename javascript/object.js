832e1ea2896cb87223be9111a81402af5f8316ec
JeongJinLee-github <thcedu@gmail.com>
2020-08-08 21:07:59 +0900

    restudy object of javaScript in ramfore

--- javascript/object.js
+++ javascript/object.js
@@ -2,7 +2,7 @@
 // one of the JavaScript's data types.
 // a collection of related data and/or functionality.
 // Nearly all objects in JavaScript are instances of Objects
-'use strict'
+"use strict";
 // 1. Litterals and properties
 
 // const name = 'Jin';
@@ -13,51 +13,51 @@
 //     console.log(age);
 // }
 
-const obj1 = {};  //'object literal' syntax
-const obj2 = new Object();  //'object constructor' syntax
+const obj1 = {}; //'object literal' syntax
+const obj2 = new Object(); //'object constructor' syntax
 
 function print(member8) {
-    console.log(member8.name);
-    console.log(member8.age);
+  console.log(member8.name);
+  console.log(member8.age);
 }
 
-const member8 = {name : 'jin', age : 54};
+const member8 = { name: "jin", age: 54 };
 print(member8);
 
 // with JavaScript magic (dynamically typed language)
 // can add porperties later
 
-member8.hasJob = true;          // imposable add another instance
+member8.hasJob = true; // imposable add another instance
 console.log(member8.hasJob);
 
-delete member8.hasJob;          // imposable delete somting instance
+delete member8.hasJob; // imposable delete somting instance
 console.log(member8.hasJob);
 
-member8['hasJob'] = true;
+member8["hasJob"] = true;
 console.log(member8.hasJob);
 
-delete member8.hasJob;          // imposable delete somting instance
+delete member8.hasJob; // imposable delete somting instance
 console.log(member8.hasJob);
 
 // 2. computed properties
 console.log(member8.name);
-console.log(member8['name']);
-member8['hasJob'] = true;
+console.log(member8["name"]);
+member8["hasJob"] = true;
 console.log(member8.hasJob);
 
 function printValue(obj, key) {
-    // console.log(obj.key);    // no used .key >> must used [key]
-    console.log(obj[key]);
+  // console.log(obj.key);    // no used .key >> must used [key]
+  console.log(obj[key]);
 }
-printValue(member8,'name');
-printValue(member8,'age');
+printValue(member8, "name");
+printValue(member8, "age");
 
 // 3. Property value shorthand
-const person1 = {name: 'bob', age:20};
-const person2 = {name: 'stev', age:30};
-const person3 = {name: 'dave', age:40};
-const person4 = makePerson('jin',54);
-const person5 = new Person('mi',48);
+const person1 = { name: "bob", age: 20 };
+const person2 = { name: "stev", age: 30 };
+const person3 = { name: "dave", age: 40 };
+const person4 = makePerson("jin", 54);
+const person5 = new Person("mi", 48);
 
 // function makePerson(name, age) {
 //     return {
@@ -67,18 +67,18 @@ const person5 = new Person('mi',48);
 // }
 
 function makePerson(name, age) {
-    return {
-        name,
-        age
-    };
+  return {
+    name,
+    age,
+  };
 }
 
 // 4.Constructor function
 function Person(name, age) {
-    // this = {}
-    this.name = name;
-    this.age = age;
-    // return this;
+  // this = {}
+  this.name = name;
+  this.age = age;
+  // return this;
 }
 
 console.log(person3.name, person3.age);
@@ -86,57 +86,70 @@ console.log(person4.name, person4.age);
 console.log(person5.name, person5.age);
 
 // 5. in operator: property existence check (key in obj)
-console.log('name' in member8);
-console.log('sex' in member8);
-console.log('age' in member8);
+console.log("name" in member8);
+console.log("sex" in member8);
+console.log("age" in member8);
 console.log(member8.sex);
 
 // 6. for..in vs for..of
 // for (key in object) {}
-console.clear();
-for (key in member8) {
-    console.log(key);
+// console.clear();
+
+for (let key in member8) {
+  console.log(key);
 }
+for (let value in member8) {
+  console.log(value);
+}
+for (let key in member8) {
+  console.log(member8[key]);
+}
+// for (let key of member8) { // TypeError: member8 is not iterable
+//   console.log(member8[key]);
+// }
+
+console.log(member8.name);
 
 // for (value of interable)
 const array = [1, 2, 3, 4, 5];
 // for (let i = 0; i <= 4; i++){
 //     console.log(array[i]);
-for (value of array) {    
-    console.log(value);
+for (let value of array) {
+  console.log(value);
 }
 
 // 7. Fun cloning
 // Object.assign(dest, [boj1, obj2, obj3....])
-const user = {name: 'jin', age:54};
+const user = { name: "jin", age: 54 };
+// const user = { name: "mi", age: 47 }; // 같은 객체명으로 객체를 사용하면 배열로 처리 되는게 아니라 ncaught SyntaxError: Identifier 'user' has already been declared
 console.log(user);
 const user2 = user;
-user2.name = 'Programmer';
+user2.name = "Programmer";
 console.log(user2);
 console.log(user);
 
 // old way
 
 const user3 = {};
-for (key in user) {
-    user3[key] = user[key];
+for (let key in user) {
+  user3[key] = user[key];
 }
-user3.name = 'coder';
+user3.name = "coder";
 console.log(`user3.name: ${user3.name}`);
 console.log(`user2.name: ${user2.name}`);
-console.log(`user.name: ${user.name}`)
+console.log(`user.name: ${user.name}`);
 
 const user4 = {};
 Object.assign(user4, user);
 console.log(user4);
-user4.name = 'lovelyMan';
+user4.name = "lovelyMan";
 console.log(user4);
 console.log(user);
 
 // another example
-const fruit1 = {color: 'red'};
-const fruit2 = {color: 'blue', size: 'big'};
-const mixed = Object.assign({}, fruit1, fruit2, {color: 'black'});
+const fruit1 = { color: "red" };
+const fruit2 = { color: "blue", size: "big" };
+const mixed = Object.assign({}, fruit1, fruit2, { color: "black" });
 console.log(mixed);
 console.log(mixed.color);
 console.log(mixed.size);
